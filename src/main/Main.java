573c50fc624ed068ca89a1af22b582a831f6cc98
Lexiana <schaller.lexi@gmail.com>
2024-11-11 17:14:27 +0100

    Init

---
+++ src/main/Main.java
@@ -0,0 +1,19 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Simple Chess");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+
+        // Add the game panel to the window
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+}
